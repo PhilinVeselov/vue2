888cc14472c6ae23826d95c04e3d8e3a6bb9fa4f
PhilinVeselov <fila.veselov21092004@gmail.com>
2025-10-07 07:40:58 +0300

    t

---
+++ vue/src/components/HeaderComponent.vue
@@ -0,0 +1,31 @@
+<template>
+  <header>
+    <nav><a href="">главная</a></nav>
+    <nav><a href="">О нас</a></nav>
+    <nav><a href="">что то</a></nav>
+  </header>
+</template>
+<style scoped lang="scss">
+header{
+  display: flex;
+  justify-content: center;
+  color: aquamarine;
+  background-color: gray;
+nav{
+  margin-right: 30px;
+  a{
+    font-size: 20px;
+    text-decoration: none;
+    color: greenyellow
+  }
+}
+}
+
+</style>
+<script setup>
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+
+</script>
---
+++ vue/src/layouts/MainLayout.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="app-container">
+    <HeaderComponent class="header" />
+
+    <div class="layout">
+      
+
+      <div class="main-content">
+        <router-view />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import HeaderComponent from "@/components/HeaderComponent.vue";
+</script>
+
+<style scoped lang="scss">
+.app-container {
+  height: 100%;
+  display: flex;
+  
+  flex-direction: column;
+}
+
+.layout {
+  display: flex;
+  flex: 1;
+  overflow-y: auto;
+}
+
+.sidebar {
+  width: 16%;
+  flex-shrink: 0;
+}
+
+.main-content {
+  flex: 1;
+  overflow-y: auto;
+  display: flex;
+  flex-direction: column;
+}
+</style>
